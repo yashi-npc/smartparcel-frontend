9ffe3db7c159a4ea2fa1dc112e74ebc8bb3e9b91
yashi-npc <yashaswinikanaujia@gmail.com>
2026-06-20 16:47:41 +0530

    blud part 7

--- src/components/hello.c
+++ src/components/hello.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main() {
-    printf("teri meri sdfkljfkmerihgghghg wekjfwefklwjfekwfjkjhf teri prem kahani testing!\n");
+    printf("teri meiuefiuehfri sdfkljfkmerihgghghg wekjfwefklwjfekwfjkjhf teri prem kahani testing!\n");
     return 0;
 }
